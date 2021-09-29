e44fa297dd9437bb00b788d3f58f8391e46d4ef5
theaniartist <wsly@wpi.edu>
2021-09-29 16:56:56 -0400

    Todos fully functional

--- build/App.js
+++ build/App.js
@@ -1,8 +1,57 @@
 import React from "./_snowpack/pkg/react.js";
-class App extends React.Component {
+class Todo extends React.Component {
   render() {
-    const {name, test} = this.props;
-    return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Hello ", name, ". Test: ", test));
+    return /* @__PURE__ */ React.createElement("li", null, this.props.name, " :", /* @__PURE__ */ React.createElement("input", {
+      type: "checkbox",
+      defaultChecked: this.props.completed,
+      onChange: (e) => this.change(e)
+    }));
+  }
+  change(e) {
+    this.props.onclick(this.props.name, e.target.checked);
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos: []};
+    this.load();
+  }
+  load() {
+    fetch("/read", {method: "get", "no-cors": true}).then((response) => response.json()).then((json) => {
+      this.setState({todos: json});
+    });
+  }
+  render() {
+    return /* @__PURE__ */ React.createElement("div", {
+      className: "App"
+    }, /* @__PURE__ */ React.createElement("input", {
+      type: "text"
+    }), /* @__PURE__ */ React.createElement("button", {
+      onClick: (e) => this.add(e)
+    }, "add"), /* @__PURE__ */ React.createElement("ul", null, this.state.todos.map((todo, i) => /* @__PURE__ */ React.createElement(Todo, {
+      key: i,
+      name: todo.name,
+      completed: todo.completed,
+      onclick: this.toggle
+    }))));
+  }
+  toggle(name, completed) {
+    fetch("/change", {
+      method: "POST",
+      body: JSON.stringify({name, completed}),
+      headers: {"Content-Type": "application/json"}
+    });
+  }
+  add(evt) {
+    const value = document.querySelector("input").value;
+    fetch("/add", {
+      method: "POST",
+      body: JSON.stringify({name: value, completed: false}),
+      headers: {"Content-Type": "application/json"}
+    }).then((response) => response.json()).then((json) => {
+      this.setState({todos: json});
+    });
   }
 }
 export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,75 @@
 
 import React from "react";
 
-class App extends React.Component {
+// we could place this Todo component in a separate file, but it's
+// small enough to alternatively just include it in our App.js file.
+
+class Todo extends React.Component {
+  // our .render() method creates a block of HTML using the .jsx format
   render() {
-    const { name, test } = this.props;
+    return <li>{this.props.name} : 
+      <input type="checkbox" defaultChecked={this.props.completed} onChange={ e => this.change(e) }/>
+    </li>
+  }
+  // call this method when the checkbox for this component is clicked
+  change(e) {
+    this.props.onclick( this.props.name, e.target.checked )
+  }
+}
+
+// main component
+class App extends React.Component {
+  constructor( props ) {
+    super( props )
+    // initialize our state
+    this.state = { todos:[] }
+    this.load()
+  }
+
+  // load in our data from the server
+  load() {
+    fetch( '/read', { method:'get', 'no-cors':true })
+      .then( response => response.json() )
+      .then( json => {
+         this.setState({ todos:json }) 
+      })
+  }
+
+  // render component HTML using JSX 
+  render() {
     return (
-      <>
-        <h1>
-          Hello {name}. Test: {test}
-        </h1>
-      </>
-    );
+      <div className="App">
+      <input type='text' /><button onClick={ e => this.add( e )}>add</button>
+        <ul>
+          { this.state.todos.map( (todo,i) => <Todo key={i} name={todo.name} completed={todo.completed} onclick={ this.toggle } /> ) }
+       </ul> 
+      </div>
+    )
+  }
+
+   // when an Todo is toggled, send data to server
+   toggle( name, completed ) {
+    fetch( '/change', {
+      method:'POST',
+      body: JSON.stringify({ name, completed }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+ 
+  // add a new todo list item
+  add( evt ) {
+    const value = document.querySelector('input').value
+
+    fetch( '/add', { 
+      method:'POST',
+      body: JSON.stringify({ name:value, completed:false }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then( response => response.json() )
+    .then( json => {
+       // changing state triggers reactive behaviors
+       this.setState({ todos:json }) 
+    })
   }
 }
 
